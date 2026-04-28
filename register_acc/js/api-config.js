8b7eb4e3adb387e6cb5613f08a1645310bc51b42
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-04-28 15:27:14 +0700

    Update api-config.js

--- register_acc/js/api-config.js
+++ register_acc/js/api-config.js
@@ -1,5 +1,5 @@
 /* version 001-3
  */
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyN3iuz3unM-obSlp08vqtuIFnuJ7gkR4opzHEtD5x7NEIzLD94g8uB_8QMo7vSFX7x/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwSts47UICrCmQ7HLxYma-TMANPTiuYxBJ02mh_hZxufpHQNgwTWEIyh1eVKjUSFNhn/exec"
 };
