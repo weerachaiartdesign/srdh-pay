e1961665be530daa205a4bf53b611f84d2ac53c5
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-04-28 13:28:50 +0700

    Update api-config.js

--- register_acc/js/api-config.js
+++ register_acc/js/api-config.js
@@ -1,5 +1,5 @@
 /* version 001-2
  */
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwkBwAh5nS4JuNNThJOwGuSgszU7VZDdiWVIsE67yaGA4tUoBMnaYOchy_JQLTOgrfg/exec"
 };
