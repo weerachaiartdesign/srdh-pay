90078087f7934f437c6bc18914f9c480d9d03a60
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-04-28 16:54:46 +0700

    003-Update api-config.js

--- register_acc/js/api-config.js
+++ register_acc/js/api-config.js
@@ -1,13 +1,5 @@
-/**
- * Configuration file for the Web App
- * Replace the URL with your Google Apps Script Web App URL
+/* version 003
  */
-
-const API_CONFIG = {
-    // นำ URL ที่ได้จากการ Deploy Google Apps Script มาใส่ที่นี่
-    scriptUrl: "https://script.google.com/macros/s/AKfycbwqkcm_ey9EZQMIEUXbnu5pCoJ0O3r81oh1Yxk33_vgpjifvCRlYB0YDCmop1TIlNRC/exec",
-    
-    // ตั้งค่าอื่นๆ เพิ่มเติมได้
-    appName: "Register Database System",
-    refreshInterval: 0 // (วินาที) 0 คือไม่รีเฟรชอัตโนมัติ
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbwvN6RQaQa7NBoBHDPrTGxpz_ZKKiHt8GDqaa8JF_nPUXPoSIKS6duAw53YUqdTIniqdw/exec"
 };
