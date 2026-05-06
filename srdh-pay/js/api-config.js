fcc729d502c89dce4f1512575d406ff0c1dd3328
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-05-06 17:54:14 +0700

    001061-Update api-config.js

--- srdh-pay/js/api-config.js
+++ srdh-pay/js/api-config.js
@@ -5,6 +5,6 @@
 // หมายเหตุ: ให้นำ URL ที่ได้จากขั้นตอน "Deploy as Web App" ใน GAS มาใส่แทนที่ xxxx
 
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxAWyQ58_EKx04UG5wH9ZpSNNBulWhDqWPowgqbyh0-Pd0WLhazDeU_FhOX0GvRzQvqUA/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzL5fuYzXXZ7YGbbeVchLzHGl9JOPzPjTytmCvFYBH-ISVh3iG2DfLHS4T4V-GwPmpL0Q/exec"
   // ตัวอย่าง: "https://script.google.com/macros/s/.../exec"
 };
