f63be680af7a2268b7535314b52699c460626076
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-05-08 17:05:36 +0700

    00132-Update API_URL to new deployment URL

--- srdh-pay/js/api-config.js
+++ srdh-pay/js/api-config.js
@@ -2,5 +2,5 @@
 
 var CONFIG = {
   // เปลี่ยนเป็น URL ของ Web App หลัง Deploy แล้ว
-  API_URL: "https://script.google.com/macros/s/AKfycbwERB_r0RHYo9OBferOZWJaTMRm8U7DLZU80cTnU-Lar9_ldKAZp5ftg9QGPdZowHu-Ow/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxlfOHTykttA2y6tGbDGmJLtbfxAkSyZ-P7-3ZX1iDnvxgGoVYXjlfcxsnjf36AhO_GYg/exec"
 };
