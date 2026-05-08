7754af161f9dcb93861ed853bd2262b33720c857
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-05-08 14:13:17 +0700

    00112-Update api-config.js

--- srdh-pay/js/api-config.js
+++ srdh-pay/js/api-config.js
@@ -1,6 +1,6 @@
-// Version 00111
+// Version 00112
 
-const CONFIG = {
+var CONFIG = {
   // เปลี่ยนเป็น URL ของ Web App หลัง Deploy แล้ว
-  API_URL: "https://script.google.com/macros/s/AKfycbyslEfOQBIRC4KZTM3zSA2qKzfM-CT8l5wGP2zCBSm4blkPOLWCq1tHtEcK90AxbMHMsA/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbz-spoRRRjC179byHNx8_MHC1ssFvEQR0uD5aUu2X8R4tf1w53d5gCL2fduiUokmzsNSQ/exec"
 };
